88cbc04b558f3728eed7aefa6efaf91020f08ea3
Joseph-Akongo <30053863@tafe.wa.edu.au>
2022-05-30 13:07:23 +0800

    File open not working

--- Vehicle Registration Mangement/Vehicle Registration Mangement.Designer.cs
+++ Vehicle Registration Mangement/Vehicle Registration Mangement.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.Enter = new System.Windows.Forms.Button();
             this.Delete = new System.Windows.Forms.Button();
             this.Edit = new System.Windows.Forms.Button();
@@ -37,21 +36,18 @@
             this.BinarySearch = new System.Windows.Forms.Button();
             this.LinearSearch = new System.Windows.Forms.Button();
             this.TAG = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.StatusLable = new System.Windows.Forms.StatusStrip();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(22, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(179, 433);
-            this.listBox1.TabIndex = 0;
             // 
             // Enter
             // 
-            this.Enter.Location = new System.Drawing.Point(270, 94);
+            this.Enter.Location = new System.Drawing.Point(243, 94);
             this.Enter.Name = "Enter";
             this.Enter.Size = new System.Drawing.Size(143, 46);
             this.Enter.TabIndex = 1;
@@ -61,25 +57,27 @@
             // 
             // Delete
             // 
-            this.Delete.Location = new System.Drawing.Point(270, 146);
+            this.Delete.Location = new System.Drawing.Point(243, 146);
             this.Delete.Name = "Delete";
             this.Delete.Size = new System.Drawing.Size(143, 46);
             this.Delete.TabIndex = 2;
             this.Delete.Text = "Delete";
             this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
             // 
             // Edit
             // 
-            this.Edit.Location = new System.Drawing.Point(270, 198);
+            this.Edit.Location = new System.Drawing.Point(243, 198);
             this.Edit.Name = "Edit";
             this.Edit.Size = new System.Drawing.Size(143, 46);
             this.Edit.TabIndex = 3;
             this.Edit.Text = "Edit";
             this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.Edit_Click);
             // 
             // Save
             // 
-            this.Save.Location = new System.Drawing.Point(270, 399);
+            this.Save.Location = new System.Drawing.Point(243, 392);
             this.Save.Name = "Save";
             this.Save.Size = new System.Drawing.Size(143, 46);
             this.Save.TabIndex = 5;
@@ -88,7 +86,7 @@
             // 
             // Open
             // 
-            this.Open.Location = new System.Drawing.Point(478, 94);
+            this.Open.Location = new System.Drawing.Point(454, 94);
             this.Open.Name = "Open";
             this.Open.Size = new System.Drawing.Size(143, 46);
             this.Open.TabIndex = 6;
@@ -98,16 +96,17 @@
             // 
             // BinarySearch
             // 
-            this.BinarySearch.Location = new System.Drawing.Point(478, 198);
+            this.BinarySearch.Location = new System.Drawing.Point(454, 198);
             this.BinarySearch.Name = "BinarySearch";
             this.BinarySearch.Size = new System.Drawing.Size(143, 46);
             this.BinarySearch.TabIndex = 7;
             this.BinarySearch.Text = "Binary Search";
             this.BinarySearch.UseVisualStyleBackColor = true;
+            this.BinarySearch.Click += new System.EventHandler(this.BinarySearch_Click);
             // 
             // LinearSearch
             // 
-            this.LinearSearch.Location = new System.Drawing.Point(478, 250);
+            this.LinearSearch.Location = new System.Drawing.Point(454, 250);
             this.LinearSearch.Name = "LinearSearch";
             this.LinearSearch.Size = new System.Drawing.Size(143, 46);
             this.LinearSearch.TabIndex = 8;
@@ -116,36 +115,85 @@
             // 
             // TAG
             // 
-            this.TAG.Location = new System.Drawing.Point(270, 250);
+            this.TAG.Location = new System.Drawing.Point(243, 250);
             this.TAG.Name = "TAG";
             this.TAG.Size = new System.Drawing.Size(143, 46);
             this.TAG.TabIndex = 9;
             this.TAG.Text = "TAG";
             this.TAG.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // Reset
             // 
-            this.button1.Location = new System.Drawing.Point(478, 399);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 46);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Reset.Location = new System.Drawing.Point(454, 392);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(143, 46);
+            this.Reset.TabIndex = 10;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // InputBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(270, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(143, 20);
-            this.textBox1.TabIndex = 11;
+            this.InputBox.Location = new System.Drawing.Point(243, 56);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(143, 20);
+            this.InputBox.TabIndex = 11;
+            // 
+            // StatusLable
+            // 
+            this.StatusLable.Location = new System.Drawing.Point(0, 467);
+            this.StatusLable.Name = "StatusLable";
+            this.StatusLable.Size = new System.Drawing.Size(620, 22);
+            this.StatusLable.TabIndex = 12;
+            this.StatusLable.Text = "statusStrip1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 407);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 445);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Selected";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 445);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Vehicle Plate Details";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 466);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(620, 489);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.StatusLable);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.Reset);
             this.Controls.Add(this.TAG);
             this.Controls.Add(this.LinearSearch);
             this.Controls.Add(this.BinarySearch);
@@ -154,7 +202,6 @@
             this.Controls.Add(this.Edit);
             this.Controls.Add(this.Delete);
             this.Controls.Add(this.Enter);
-            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -163,8 +210,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button Enter;
         private System.Windows.Forms.Button Delete;
         private System.Windows.Forms.Button Edit;
@@ -173,8 +218,13 @@
         private System.Windows.Forms.Button BinarySearch;
         private System.Windows.Forms.Button LinearSearch;
         private System.Windows.Forms.Button TAG;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.StatusStrip StatusLable;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Vehicle Registration Mangement/Vehicle Registration Mangement.cs
+++ Vehicle Registration Mangement/Vehicle Registration Mangement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace Vehicle_Registration_Mangement
         {
             InitializeComponent();
         }
+        List<String> myArray = new List<string>();
 
         private void Open_Click(object sender, EventArgs e)
         {
@@ -23,16 +25,70 @@ namespace Vehicle_Registration_Mangement
             plateFile.Title = "Open Text File";
             plateFile.Filter = "TXT files|*.txt";
             plateFile.InitialDirectory = @"C:\Users\30053863\Source\Repos\Vehicle Registration Mangement";
+            OpenFileDialog f = new OpenFileDialog();
             if (plateFile.ShowDialog() == DialogResult.OK)
             {
+                listBox1.Items.Clear();
+                List<String> list = new List<String>();
+                using (StreamReader r = new StreamReader(f.OpenFile()))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(line);
+                    }
+                }
                 MessageBox.Show(plateFile.FileName.ToString());
             }
+        }
+
+        private void DisplayPlate()
+        {
+            myArray.Clear();
         }
 
 
         private void Enter_Click(object sender, EventArgs e)
         {
-
+            if (string.IsNullOrEmpty(InputBox.Text))
+                return;
+                listBox1.Items.Add(InputBox.Text);
+                InputBox.Clear();
+                InputBox.Focus();
+            
         }
+
+        private void BinarySearch_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Edit_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = listBox1.Text;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Count > 0)
+               listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                InputBox.Clear();
+                InputBox.Focus();
+            }
+        }
+
+
     }
 }
+
